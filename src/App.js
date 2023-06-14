9a5c268cd9ca25f46cbc4e7fd4f5bab23075167d
pol <pablitu2010@live.com.ar>
2023-06-14 18:33:30 -0300

    probando react-3

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>vamos pol!!!!</h1>
         <p>
+
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
