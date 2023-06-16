ba2fdaf1f6dd3e263572f6a7b6cc10c2e0432ba4
pol <pablitu2010@live.com.ar>
2023-06-16 18:56:04 -0300

    modificando react-3 para subirlo a internet

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 function App() {
   return (
+    
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -17,7 +19,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Aprende React bolu!!! no pierdas el tiempo.
+          Aprende React bolu!!! no pierdas el tiempo.Dicen que los salarios van desde los 179.000 pesos argentinos, si!!!! .<strong>qué estás esperando !!!</strong>
         </a>
       </header>
     </div>
